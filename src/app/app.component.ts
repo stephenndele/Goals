6e481762672aad35570480edaf37ea10a28caac4
Stephen Ndele <stephenndele09346@gmail.com>
2021-01-29 16:37:55 +0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Goal } from './goal';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Goals';
+  title(title: any) {
+    throw new Error('Method not implemented.');
+  }
+
 }
